31810ad582887eae649d3c92681a389bd4f0a677
stepkobetsu-hub <stepkobetsu@gmail.com>
2026-08-03 21:11:28 +0900

    Phase 4: type dual-write bindings

--- cloudflare/src/env.d.ts
+++ cloudflare/src/env.d.ts
@@ -3,5 +3,8 @@ Wall time: 1.1 seconds
 Output:
 interface Env {
   MIRROR_COMPARE_TOKEN: string;
+  GOOGLE_DUAL_WRITE_TOKEN: string;
+  GOOGLE_DUAL_WRITE_URL: string;
+  DUAL_WRITE_ENABLED: string;
 }
 
