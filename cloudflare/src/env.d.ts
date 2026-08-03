fb5a2e2dff4849681043cfffc553128b57d3c2d6
stepkobetsu-hub <stepkobetsu@gmail.com>
2026-08-03 21:36:15 +0900

    Phase 4.5: batch Google backup synchronization

--- cloudflare/src/env.d.ts
+++ cloudflare/src/env.d.ts
@@ -1,10 +1,11 @@
 Exit code: 0
-Wall time: 1.1 seconds
+Wall time: 4 seconds
 Output:
 interface Env {
   MIRROR_COMPARE_TOKEN: string;
   GOOGLE_DUAL_WRITE_TOKEN: string;
   GOOGLE_DUAL_WRITE_URL: string;
   DUAL_WRITE_ENABLED: string;
+  SYNC_BATCH_SIZE: string;
 }
 
